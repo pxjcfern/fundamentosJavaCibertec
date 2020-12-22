5b278770f62972ca256d755914a7d8bf9bfa4db0
Adminfoco <Adminfoco@10.0.0.15>
2020-12-21 22:14:15 -0500

    Prueba inicial

--- src/Paquetes/HolaMundo.java
+++ src/Paquetes/HolaMundo.java
@@ -3,7 +3,7 @@ package Paquetes;
 // @author Adminfoco
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo...");
+        System.out.println("Hola mundo!!!");
     }
 }
 
