1c77bf914b507aea6ed61a3dfcb38186e09da77e
Hp <Hp@INSPIRA>
2020-12-23 14:07:19 -0500

    v2

--- src/pruebas/Pruebas.java
+++ src/pruebas/Pruebas.java
@@ -3,7 +3,7 @@ package pruebas;
 public class Pruebas {
 
     public static void main(String[] args) {
-        System.out.println("Prueba 1");
+        System.out.println("Prueba 2");
     }
     
 }
